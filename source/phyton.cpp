6168b742ab323dac717aefefad8ed78069ec4659
Berke <69055194+Fsb3rKe@users.noreply.github.com>
2021-12-10 10:22:47 +0300

    Add files via upload

--- source/phyton.cpp
+++ source/phyton.cpp
@@ -7,10 +7,9 @@ int main(int argc, char const *argv[])
     std::string space = " ";
     for (int i = 0; i <= argc; i++)
     {
-        if (argv[i] == argv[0] || argv[i] == argv[1]) {
-            if (argv[i] == argv[1]) {
-                fileName = argv[1];
-            }
+        if (argv[i] == argv[0]) {}
+        else if (argv[i] == argv[1]) {
+            fileName = argv[1];
         }
         else {
             args += space+argv[i];

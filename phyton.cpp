37db7ad44f66d341988e2a567cf63effa8bc8e31
Berke <69055194+Fsb3rKe@users.noreply.github.com>
2021-12-10 10:11:55 +0300

    Add files via upload

---
+++ phyton.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+int main(int argc, char const *argv[])
+{
+    std::string fileName = "";
+    std::string args = "";
+    std::string space = " ";
+    for (int i = 0; i <= argc; i++)
+    {
+        if (argv[i] == argv[0] || argv[i] == argv[1]) {
+            if (argv[i] == argv[1]) {
+                fileName = argv[1];
+            }
+        }
+        else {
+            args += space+argv[i];
+        }
+    }
+    std::string pythonStart = "python ";
+    std::string command = pythonStart+fileName+args;
+    system(command.c_str());
+    return 0;
+}
